4929731263ec3864a0e20dd62c7c7e3605d0ce28
pritamkhose <pritamkhose@gmaio.com>
2019-08-20 02:36:46 +0530

    update Constants

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,8 @@
-
 const String BaseURL = 'https://api.github.com/';
 const String GitHubURL = 'https://github.com/';
-class Constants {
+const String APP_TAG = "GithubExplorerFlutter";
+const String EMAIL_VERIFICATION =
+    "^[a-zA-Z0-9_!#\$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+\$";
+const int CONNECTION_TIMEOUT = 30;
 
-}
+class Constants {}
