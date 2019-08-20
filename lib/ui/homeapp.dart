7908708bd09e3466773cec7f6d07de1a81c98030
pritamkhose <pritamkhose@gmaio.com>
2019-08-20 23:46:10 +0530

    Add Search PullToRefresh

--- lib/ui/homeapp.dart
+++ lib/ui/homeapp.dart
@@ -47,15 +47,74 @@ class Choice {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-//  final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
   Choice _selectedChoice = choices[0]; // The app's "state".
-  var _searchText = "";
+  var _searchText = "android";
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  List<Item> data = [];
+
+  // https://kodestat.gitbook.io/flutter/flutter-listview-with-json-or-list-data
+  Future<String> _getGitUsers() async {
+    //    var connectivityResult = await (Connectivity().checkConnectivity());
+//    // I am connected to a mobile or wifi network.
+//    if (connectivityResult == ConnectivityResult.mobile ||
+//        connectivityResult == ConnectivityResult.wifi) {
+    try {
+      var response = await http.get(Uri.encodeFull(
+          Constants.BaseURL + 'search/users?q=' + _searchText + '&page=1'));
+      var aObj = json.decode(response.body);
+      // print(aObj["items"]);
+      List<Item> adata = [];
+      for (var user in aObj["items"]) {
+        Item newUser =
+            Item(user["avatar_url"], user["login"], user["score"].toString());
+        adata.add(newUser);
+      }
+      if (adata.length == 0) {
+        _scaffoldKey.currentState.showSnackBar(SnackBar(
+          content: Text('No Search Found!'),
+        ));
+      }
+      this.setState(() {
+        data = adata;
+      });
+      return "Success!";
+    } on SocketException catch (_) {
+      print('not connected');
+//    } else {
+      final snackBar = SnackBar(
+        content: Text('No Internet connected!'),
+        action: SnackBarAction(
+          label: 'Retry',
+          onPressed: () {
+            _getGitUsers();
+          },
+        ),
+      );
+
+      // Find the Scaffold in the widget tree and use
+      // it to show a SnackBar.
+      Scaffold.of(context).showSnackBar(snackBar);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => _refreshIndicatorKey.currentState.show());
+    this._getGitUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: Text(widget.title),
           actions: <Widget>[
@@ -79,85 +138,106 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                            labelText: 'Please enter some text for Search'),
-//                        initialValue: data == null ? '' : data.title,
-                        validator: (String value) {
-                          if (value.isEmpty || value.length < 2) {
-                            return 'Please enter some text and should be 2+ characters long.';
-                          }
-                        },
-                        onSaved: (String value) {
-                          _searchText = value;
-                        },
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                                decoration: InputDecoration(
+                                    labelText:
+                                        'Please enter some text for Search'),
+                                initialValue: '',
+                                validator: (String value) {
+                                  if (value.isEmpty || value.length < 2) {
+                                    return 'Please enter some text and should be 2+ characters long.';
+                                  } else {
+                                    _searchText = value;
+                                    _getGitUsers();
+                                  }
+                                },
+                                onSaved: (String value) {
+                                  print('onSaved ' + value);
+                                },
+                                textInputAction: TextInputAction.done,
+                                keyboardType: TextInputType.text,
+                                onFieldSubmitted: (String value) {
+                                  _formKey.currentState.validate();
+                                }),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              // close Keyboard - https://stackoverflow.com/questions/51652897/how-to-hide-soft-input-keyboard-on-flutter-after-clicking-outside-textfield-anyw
+                              FocusScope.of(context)
+                                  .requestFocus(new FocusNode());
+                              _formKey.currentState.validate();
+                            },
+                            child: Image(
+                              image:
+                                  new AssetImage("assets/images/search80.png"),
+                              width: 60,
+                              height: 60,
+                              color: null,
+                              fit: BoxFit.scaleDown,
+                              alignment: Alignment.center,
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                    Image(
-                      image: new AssetImage("assets/images/search80.png"),
-                      width: 60,
-                      height: 60,
-                      color: null,
-                      fit: BoxFit.scaleDown,
-                      alignment: Alignment.center,
-                    )
                   ],
                 ),
               ),
+/*              Column(
+                children: <Widget>[
+                  Visibility(
+                    child: Text(
+                      'No Search Found!',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    visible: data.length == 0,
+                  ),
+                  Visibility(
+                    visible: data.length != 0,
+                    child:*/
               Expanded(
-                child: FutureBuilder(
-                  future: _getGitUsers(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.data == null) {
-                      return Container(
-                        child: Center(
-                          //child: Text("Loading..."),
-                          child: CircularProgressIndicator(),
-                        ),
-                        /* child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            new CircularProgressIndicator(),
-                            new Text("Loading...")
-                          ],
-                        ),*/
-                      );
-                    } else {
-                      return Container(
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Card(
-                              child: ListTile(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      new MaterialPageRoute(
-                                          builder: (context) => UserDetailPage(
-                                              snapshot.data[index])));
-                                },
-                                title: Text(snapshot.data[index].login),
-                                subtitle: Text(
-                                    "Score : " + snapshot.data[index].score),
-                                leading: Image(
-                                    image: NetworkImage(
-                                        snapshot.data[index].avatar_url)),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    }
-                  },
+                child: Container(
+                  child: RefreshIndicator(
+                    key: _refreshIndicatorKey,
+                    onRefresh: _getGitUsers,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: data == null ? 0 : data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          child: ListTile(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) =>
+                                          UserDetailPage(data[index])));
+                            },
+                            title: Text(data[index].login),
+                            subtitle: Text("Score : " + data[index].score),
+                            leading: Image(
+                                image: NetworkImage(data[index].avatar_url)),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
               ),
+              /*  ),
+                ],
+              ),*/
             ],
           ),
         ),
@@ -177,44 +257,5 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<bool> _onWillPop() {
     return Utility().onWillPop(context);
-  }
-
-  @override
-  Future<List<Item>> _getGitUsers() async {
-//    var connectivityResult = await (Connectivity().checkConnectivity());
-//    // I am connected to a mobile or wifi network.
-//    if (connectivityResult == ConnectivityResult.mobile ||
-//        connectivityResult == ConnectivityResult.wifi) {
-    try {
-      List<Item> users = [];
-      var response =
-          await http.get(Constants.BaseURL + 'search/users?q=pritam&page=1');
-
-      var aObj = json.decode(response.body);
-//    print(aObj["items"][10]["avatar_url"]);
-      for (var user in aObj["items"]) {
-        Item newUser =
-            Item(user["avatar_url"], user["login"], user["score"].toString());
-        users.add(newUser);
-      }
-      return users;
-    } on SocketException catch (_) {
-      print('not connected');
-
-//    } else {
-      final snackBar = SnackBar(
-        content: Text('No Internet connected!'),
-        action: SnackBarAction(
-          label: 'Retry',
-          onPressed: () {
-            _getGitUsers();
-          },
-        ),
-      );
-
-      // Find the Scaffold in the widget tree and use
-      // it to show a SnackBar.
-      Scaffold.of(context).showSnackBar(snackBar);
-    }
   }
 }
